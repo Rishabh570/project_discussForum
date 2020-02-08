558f03dfaafa5ca2181d42dad6fb0a25525520bd
karansehgal15 <karansehgal151998@gmail.com>
2020-02-09 00:41:10 +0530

    ok

--- backend/src/routers/users.js
+++ backend/src/routers/users.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => {
 	db.users.findAll()
 	.then(users => {
 		console.log("users => ", users);
+		res.send(users);
 		res.sendStatus(200);
 	})
 	.catch(err => {
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,11 @@
 const express = require('express')
 	, signupRoute = require('./routers/signup')
-	, userRoutes = require('./routers/users')
-	, bodyParser = require('body-parser');
+	, userRoutes = require('./routers/users');
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 
 // TEST DB
 const db = require('./db/database')
@@ -14,10 +13,11 @@ db.authenticate()
 .then(() => {console.log("Database Connected.")})
 .catch(err => console.log("Error in DB connection: ", err));
 
+//direction to desired routes
 
 app.use('/signup', signupRoute);
 app.use('/users', userRoutes);
 
-
+//activation of port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
