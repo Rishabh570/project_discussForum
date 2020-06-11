b1ab47e26ddab519ce983c72e4e2542c7a3e8e84
karansehgal15 <karansehgal151998@gmail.com>
2020-06-11 17:16:09 +0530

    updated databases.js

--- src/db/database.js
+++ src/db/database.js
@@ -14,5 +14,13 @@ const sequelize = require('sequelize')
  * THIS IS THE PRODUCTION DATABASE
  */
 module.exports = new sequelize('heroku_42d990be20d6c89', 'b6419cb2460fc8', '85937b9d', {
-	host: 'us-cdbr-east-05.cleardb.net'
+	host: 'us-cdbr-east-05.cleardb.net',
+	dialect: 'mysql',
+	port: 3306,
+	pool: {
+		max: 5,
+		min: 0,
+		idle: 60000,
+		acquire: 120000
+	}
 });
