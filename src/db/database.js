69a450f11df6df0266bf4a6b955d920eea4da6e8
karansehgal15 <karansehgal151998@gmail.com>
2020-06-11 14:35:22 +0530

    dep

--- src/db/database.js
+++ src/db/database.js
@@ -14,7 +14,5 @@ const sequelize = require('sequelize')
  * THIS IS THE PRODUCTION DATABASE
  */
 module.exports = new sequelize('heroku_42d990be20d6c89', 'b6419cb2460fc8', '85937b9d', {
-	host: 'us-cdbr-east-05.cleardb.net',
-	port: 3306,
-	dialect: 'mysql',
+	host: 'us-cdbr-east-05.cleardb.net'
 });
