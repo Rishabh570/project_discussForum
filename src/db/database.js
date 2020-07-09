95ed4082ca2c1e83cf5d636a04f7a421149ec1f8
karansehgal15 <karansehgal151998@gmail.com>
2020-07-09 12:33:02 +0530

    hh

--- src/db/database.js
+++ src/db/database.js
@@ -4,26 +4,22 @@ const Sequelize = require('sequelize')
  * THIS IS FOR DEVELOPMENT
  * UN-COMMENT THIS DURING DEVELOPING ON LOCAL
  */
-// 
-const db = new Sequelize('db', 'founder', 'ksksrrpj01@', {
-// 
-	host: 'localhost',
-//
- 	dialect: 'mysql',
-//
- });
+// const db = new Sequelize('db', 'founder', 'ksksrrpj01@', {
+// host: 'localhost',
+//dialect: 'mysql',
+// });
 
 
 /**
  * THIS IS THE PRODUCTION DATABASE
  */
-/*
+
 const db = new Sequelize('heroku_42d990be20d6c89', 'b6419cb2460fc8', '85937b9d', {
 	host: "us-cdbr-east-05.cleardb.net",
 	dialect: 'mysql',
 	port: 3306,
 });
-*/
+
 
 module.exports = {
 	db
